9494ab4b4252b6d9e3a45f450db3c3ee78a842a1
Agbamajnr <ulimhunyieagbama@gmail.com>
2022-04-17 22:21:49 +0100

    add balance calculation

---
+++ src/components/Deposit.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="deposit">
+      <div class="modal w-full h-full col-flex items-center gap-y-9 p-7">
+          <div class="row-flex justify-between w-full">
+              <p class="text-xl font-bold w-full text-left">Deposit Tron(TRX)</p>
+              <svg @click="$emit('closeModal')" class="w-6 h-6 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+          </div>
+          <qrcode-vue value="ulimhunyieagbama@gmail.com" level="H" size="230"></qrcode-vue>
+          <div class="address row-flex relative w-full h-full items-center">
+              <p class="text-sm text-left pl-2">TXoyJrBLgXBJYcrA9mVrXoTWyDQ1oVrKux</p>
+              <button class="copy absolute right-1 bottom-1 text-white text-center" @click="copyAddress"><p v-if="addressCopied === false">Copy</p><p v-if="addressCopied === true">Copied</p></button>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import {ref, reactive} from 'vue'
+import QrcodeVue from 'qrcode.vue'
+import copy from 'copy-to-clipboard3';
+export default {
+    components: {
+    QrcodeVue,
+  },
+  setup() {
+      const addressCopied = ref(false)
+      const copyAddress = () => {
+          copy('This Address')
+          addressCopied.value = true;
+      }
+
+      return {copyAddress, addressCopied}
+  }
+}
+</script>
+
+<style scoped>
+    .deposit {
+        width: 482px;
+        height: 467px;
+        opacity: 1;
+    }
+    .modal {
+        background-color: #ffffff;
+        opacity: 1;
+    }
+    .address {
+        background: #F5FEF9;
+        border: 1px solid #08C052;
+        box-sizing: border-box;
+        height: 45px;
+    }
+    .copy {
+        background: #08C052;
+        width: 80px;
+        height: 35px;
+    }
+</style>
